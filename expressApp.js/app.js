dd7c3b798fb24774aa76ed296320f1c2dc7c9246
yanaazaryan <57559475+yanaazaryan@users.noreply.github.com>
2022-03-30 23:14:04 +0400

    Update app.js

--- expressApp.js/app.js
+++ expressApp.js/app.js
@@ -1,13 +1,13 @@
 const asyncHandler = require('express-async-handler');
 const errorHandler = require('../middleware/error-handler.middleware.js ');
-const citiesController = require('../cities//cities.controller')
+const citiesController = require('../cities/cities.controller');
 const express = require("express");
 const app = express();
 
 app.use('/cities', citiesController);
 
 app.use((err, req, res) => {
-    res.status(500).send(err.message)
+    res.status(500).send(err.message);
 })
 
 app.listen(3000, () => {
